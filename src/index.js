f1aeeb055092829a12c0c840eec6661e60860b9b
Austin Wood <i@austinwood.me>
2016-09-28 20:48:20 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import StorePicker from './components/store-picker'
+
+render(<StorePicker></StorePicker>, document.getElementById('main'))
