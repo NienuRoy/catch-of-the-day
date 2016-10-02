a3d1432ada7db991ecdd2bd38867ddbf05449e7f
Austin Wood <i@austinwood.me>
2016-10-01 19:38:05 -0700

    Create app layout with components

--- src/components/store-picker.js
+++ src/components/store-picker.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { getFunName } from '../helpers'
 
 export default class StorePicker extends React.Component {
+  goToStore(event) {
+    event.preventDefault()
+    const storeId = this.storeInput.value
+  }
+
+  // `this` in a component lifecycle method (like `render`) is bound to the
+  // component instance; all other custom methods are not bound by default
   render() {
-    return <p>Hello</p>
+    return (
+      <form className="store-selector" onSubmit={e => this.goToStore(e)}>
+        <h2>Please Enter A Store</h2>
+        {/* function refs > string refs  */}
+        <input type="text" required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={input => this.storeInput = input}
+        />
+        <button type="submit">Visit Store</button>
+      </form>
+    )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
+import './css/style.css'
 
 import StorePicker from './components/store-picker'
 
-render(<StorePicker></StorePicker>, document.getElementById('main'))
+render(<StorePicker />, document.getElementById('main'))
