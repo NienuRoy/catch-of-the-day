79e78c0f22225d8ab6684ce8d20abf62c4668c67
Austin Wood <i@austinwood.me>
2016-10-02 10:40:21 -0700

    Use props and state to handle adding a fish to our fishes state

--- src/components/app.js
+++ src/components/app.js
@@ -5,6 +5,21 @@ import Order from './order'
 import Inventory from './inventory'
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.addFish = this.addFish.bind(this)
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+
+  addFish(fish) {
+    const fishes = Object.assign({}, this.state.fishes)
+    fishes[`fish-${Date.now()}`] = fish
+    this.setState({ fishes })
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -12,7 +27,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
